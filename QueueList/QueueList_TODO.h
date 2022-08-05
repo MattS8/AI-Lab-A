38b79ba7067353326755310a9341f55119fccf97
Matthew Steinhardt <Matthew.steinhardt@gmail.com>
2022-08-05 14:26:56 -0700

    QueueList

--- QueueList/QueueList_TODO.h
+++ QueueList/QueueList_TODO.h
@@ -14,11 +14,21 @@ namespace fullsail_ai { namespace fundamentals {
 	//!   - Define the <code>QueueList</code> destructor.
 	//!   - Define the <code>QueueList</code> methods.
 
+	template<typename T>
+	inline QueueList<T>::QueueList()
+	{
+		front = back = nullptr;
+	}
+
+	template<typename T>
+	inline QueueList<T>::~QueueList()
+	{
+		removeAll();
+	}
 
 	template <typename T>
 	typename QueueList<T>::Iterator& QueueList<T>::Iterator::operator++()
 	{
-		// TODO: Complete the definition of this operator.
 		if (this->currentNode != nullptr)
 			this->currentNode = this->currentNode->next;
 		return *this;
@@ -27,8 +37,116 @@ namespace fullsail_ai { namespace fundamentals {
 	template <typename T>
 	T QueueList<T>::Iterator::operator*() const
 	{
-		// TODO: Complete the definition of this operator.
 		return this->currentNode->data;
+	}
+
+	template<typename T>
+	inline bool QueueList<T>::isEmpty() const
+	{
+		return front == nullptr;
+	}
+
+	template<typename T>
+	inline void QueueList<T>::enqueue(T element)
+	{
+		// Create new node
+		Node* newNode = new Node(element);
+		newNode->next = nullptr;
+
+		if (isEmpty())
+			front = back = newNode;
+		else 
+		{
+			back->next = newNode;
+			back = newNode;
+		}
+	}
+
+	template<typename T>
+	inline T QueueList<T>::getFront() const
+	{
+		return front->data;
+	}
+
+	template<typename T>
+	inline void QueueList<T>::dequeue()
+	{
+		if (front == nullptr)
+			return;
+
+		Node* tempNode = front;
+		front = front->next;
+		delete tempNode;
+
+		// Set back to null if last item was dequeued
+		if (front == nullptr)
+			back = nullptr;
+	}
+
+	template<typename T>
+	inline void QueueList<T>::removeAll()
+	{
+		while (front != nullptr)
+		{
+			dequeue();
+		}
+	}
+
+	template<typename T>
+	inline bool QueueList<T>::contains(T element) const
+	{
+		for (auto it = this->begin(); it != this->end(); it++)
+		{
+			if (*it == element)
+				return true;
+		}
+
+		return false;
+	}
+
+	template<typename T>
+	inline void QueueList<T>::remove(T element)
+	{
+		if (isEmpty())
+			return;
+
+		Node* parent = front;
+		Node* current = front;
+		Node* tempNode;
+		while (current != nullptr)
+		{
+			if (current->data == element)
+			{
+				// Remove Front
+				if (current == front)
+				{
+					tempNode = front;
+					front = front->next;
+					if (front == nullptr)
+						back = nullptr;
+					delete tempNode;
+					return;
+				}
+
+				// Remove Back
+				if (current == back)
+				{
+					delete back;
+					back = parent;
+					back->next = nullptr;
+					return;
+				}
+
+				// Remove Middle
+				tempNode = current;
+				parent->next = tempNode->next;
+				delete tempNode;
+				return;
+			}
+
+			parent = current;
+			current = current->next;
+		}
 	}
 
 
